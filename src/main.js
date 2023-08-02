beaaab498d0aa8bc1bd9d9c81a753ddd86e13ef4
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-08-02 16:25:22 -0300

    header quase finalizado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
+function redirectToPage() {
+    const selectElement = document.querySelector(".categoria-select"); // Obtém o elemento <select>
+    const selectedOption = selectElement.value; // Obtém o valor da opção selecionada
+  
+    if (selectedOption && selectedOption !== "categoria") {
+      // Se a opção selecionada não for vazia e não for a opção "Categoria", redireciona para a página correspondente
+      window.location.replace(selectedOption + "");
+    }
+  }
+  
+  
 console.log(example, data);
